207635cbd32c654ed2dd4831b02648d55ffeb69d
Gnosticka <141839430+Gnosticka@users.noreply.github.com>
2023-08-19 15:59:07 +0300

    Add files via upload

---
+++ assets/QSpace.05c1b773.js
@@ -0,0 +1,1 @@
+import{c as a}from"./dom.f603a4b0.js";import{j as e}from"./index.664f10fb.js";const p=e("div",{class:"q-space"});var c=a({name:"QSpace",setup(){return()=>p}});export{c as Q};
---
+++ assets/RootPageTitle.246880f1.js
@@ -0,0 +1,1 @@
+import{a as t,L as o,o as a,B as s,a3 as _}from"./index.664f10fb.js";const n={class:"root-page-title"},p=t({__name:"RootPageTitle",setup(r){return o(),(e,i)=>(a(),s("div",n,_(e.$route.name),1))}});export{p as _};
